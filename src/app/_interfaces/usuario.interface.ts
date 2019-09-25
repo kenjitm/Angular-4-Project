f9a3a50a765695b487e5f31131a82fae328cab40
Kenji Tanaka <kenjitm@outlook.com>
2019-09-24 21:47:45 -0500

    json data model

---
+++ src/app/_interfaces/usuario.interface.ts
@@ -0,0 +1,10 @@
+export interface Usuario {
+    id: string;
+    nombre: string;
+    edad: string;
+    email: string;
+    foto: string;
+    celular: number;
+    cedula: number;
+    estado: boolean;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as USUARIOS from './../assets/data/Usuario.json';
+import { Usuario } from 'src/app/_interfaces/usuario.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  usuarios: Usuario[];
   title = 'Proyecto';
+//
+  ngOnInit(){
+    this.usuarios = USUARIOS as any;
+    console.log(this.usuarios);
+  }
 }
+
+
